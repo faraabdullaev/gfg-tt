4235b9d6bbd30b8762670931de98ae022920a1ee
Farrukh Abdullaev <faraabdullaev@gmail.com>
2019-02-02 14:47:18 +0500

    `api-v1` module generated

---
+++ api/modules/v1/Module.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\api\modules\v1;
+
+/**
+ * api module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\api\modules\v1\controllers';
+    public $defaultRoute = 'products';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
--- config/web.php
+++ config/web.php
@@ -52,6 +52,11 @@ $config = [
         ],
         */
     ],
+    'modules' => [
+        'v1' => [
+            'class' => 'app\api\modules\v1\Module',
+        ],
+    ],
     'params' => $params,
 ];
 

5a44934c6d1e95ed5a4c47c16ff1abf471ff01c3
Farrukh Abdullaev <faraabdullaev@gmail.com>
2019-02-03 15:57:44 +0500

    UI implemented:  - search form added  - assets updated  - main-JS file added  - unused pages removed

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,65 @@
 <?php
-
 /* @var $this yii\web\View */
+/* @var $brands array */
+/* @var $sortFields array */
 
-$this->title = 'My Yii Application';
+/* @var $model \app\models\ProductSearchForm */
+
+
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
+
+$this->title = 'Search Service Testing UI';
 ?>
 <div class="site-index">
+    <div class="row">
+        <p>Search Filters:</p>
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <?php $form = ActiveForm::begin([
+            'id' => 'search-form',
+            'layout' => 'horizontal',
+            'fieldConfig' => [
+                'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                'labelOptions' => ['class' => 'col-lg-1 control-label'],
+            ],
+        ]); ?>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <div class="col-lg-6">
+            <?= $form->field($model, 'title')->textInput(['autofocus' => true]) ?>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+            <?= $form->field($model, 'brand')->checkboxList($brands, [
+                'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+            ]) ?>
 
-    <div class="body-content">
+            <?= $form->field($model, 'price')->input('number', ['min' => 0.0, 'step' => 0.5]) ?>
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <?= $form->field($model, 'stock')->input('number', ['min' => 0, 'step' => 1]) ?>
+        </div>
+        <div class="col-lg-6">
+            <?= $form->field($model, 'sortBy')->radioList($sortFields) ?>
+            <?= $form->field($model, 'page')->input('number', ['min' => 1, 'step' => 1]) ?>
+        </div>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+        <?= $form->field($model, 'basicAccessToken')->hiddenInput()->label('') ?>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+        <div class="form-group">
+            <div class="col-lg-offset-1 col-lg-11">
+                <?= Html::submitButton('Search', ['class' => 'btn btn-primary', 'name' => 'search-button']) ?>
             </div>
         </div>
 
+        <?php ActiveForm::end(); ?>
+    </div>
+
+    <div id="results" style="display: none">
+        <div class="info">
+            <p>URL: <span id="url"></span></p>
+            <p>Status: <span id="status"></span></p>
+            <p>Code: <span id="code"></span></p>
+            <p>Page count: <span id="count"></span></p>
+            <p>Current page: <span id="current"></span></p>
+            <p>Total count: <span id="total"></span></p>
+        </div>
+        <pre></pre>
     </div>
 </div>
